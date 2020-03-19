58aed0ab1277554e9708b0d250f4af7c540fde17
JanoschekBalazs <janoschekb@gain.uni-sopron.hu>
2020-03-19 16:16:07 +0100

    Reworked input reading

--- src/main/Main.java
+++ src/main/Main.java
@@ -11,21 +11,27 @@ public class Main {
 
     public static void main(String[] args) {
         Stopwatch stopwatch = new Stopwatch();
+        float elapsedTime;
         stopwatch.start();
 
-        float elapsedTime;
-        ArrayList<String> words = new ArrayList<>();
         File input = new File("wordlist.txt");
-
-        try {
-            Scanner in = new Scanner(input);
-            if (in.hasNextLine())
-                words.add(in.nextLine());
-        } catch (FileNotFoundException e) {
-            System.out.println("File not found!");
-        }
+        ArrayList<String> words = read(input);
 
         elapsedTime = stopwatch.checkTime();
         System.out.println("Reading input took " + elapsedTime + " seconds.");
+    }
+
+    static ArrayList<String> read(File from) {
+        try {
+            Scanner in = new Scanner(from);
+            ArrayList<String> toReturn = new ArrayList<>();
+            while (in.hasNextLine())
+                toReturn.add(in.nextLine());
+            in.close();
+            return toReturn;
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found!");
+            return null;
+        }
     }
 }
